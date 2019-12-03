ddb6a11ce3ab72846021f28e5ad5569ea3897b4f
Naxinix <43404918+Naxinix@users.noreply.github.com>
2019-12-03 03:42:59 +0100

    Major changes

--- .cache/behavior-hx/ActorEvents_80.hx
+++ .cache/behavior-hx/ActorEvents_80.hx
@@ -79,7 +79,7 @@ class ActorEvents_80 extends ActorScript
 		{
 			if(wrapper.enabled && sameAsAny(getActorType(0), event.otherActor.getType(),event.otherActor.getGroup()))
 			{
-				switchScene(GameModel.get().scenes.get(4).getID(), null, createCrossfadeTransition(.2));
+				switchScene(GameModel.get().scenes.get(1).getID(), null, createCrossfadeTransition(.2));
 			}
 		});
 		
